14335d71dd95ba3646d7c3c7565a3b736f3b7ff0
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-06-03 13:58:43 -0500

    add new selection algorithm

--- population.py
+++ population.py
@@ -11,12 +11,9 @@ def fit2prob_sm(fitnesses, **kwargs):
 
 def calc_next_population(pop, prob, calc_clone, calc_mutate, calc_crossover=None, **kwargs):
     npop = []
-    parent_idxs = []
 
     n_elite_idxs = np.argsort(prob)[::-1][:kwargs['n_elite']]
-
     npop.extend(calc_clone(pop[n_elite_idxs]))
-    parent_idxs.extend(n_elite_idxs)
 
     n_children = len(pop)-len(npop)
     if calc_crossover is not None and kwargs['crossover']:
@@ -28,6 +25,18 @@ def calc_next_population(pop, prob, calc_clone, calc_mutate, calc_crossover=None
     else:
         children = np.random.choice(pop, size=n_children, 
                                     p=prob, replace=kwargs['with_replacement'])
+    children = calc_mutate(children)
+    npop.extend(children)
+    return np.array(npop)
+
+def calc_next_population2(pop, prob, calc_clone, calc_mutate, calc_crossover=None, n_elite=1, uniform_top_T=10):
+    npop = []
+
+    sort_idx = np.argsort(prob)[::-1]
+    npop.extend(calc_clone(pop[sort_idx[:n_elite]]))
+
+    n_children = len(pop)-len(npop)
+    children = np.random.choice(pop[sort_idx[n_elite:n_elite+uniform_top_T]], size=n_children)
     children = calc_mutate(children)
     npop.extend(children)
     return np.array(npop)
