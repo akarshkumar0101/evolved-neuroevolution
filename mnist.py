e4ee42cf35787f9cc4d7d378361a0f5137557b49
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-06-03 13:56:34 -0500

    make mnist work with batches

--- mnist.py
+++ mnist.py
@@ -11,20 +11,24 @@ class MNIST:
                                                     torchvision.transforms.Normalize((0.1307,), (0.3081,))])
 
         self.ds_train = torchvision.datasets.MNIST('./', train=True, download=True, transform=self.transform)
-        self.loader_train = torch.utils.data.DataLoader(self.ds_train, 
-                                                        batch_size=len(self.ds_train), shuffle=True)
         self.ds_test = torchvision.datasets.MNIST('./', train=False, download=True, transform=self.transform)
+        self.bs_train = 1000
+        self.bs_test = 3000
+        self.loader_train = torch.utils.data.DataLoader(self.ds_train, 
+                                                        batch_size=self.bs_train, shuffle=True)
         self.loader_test = torch.utils.data.DataLoader(self.ds_test, 
-                                                       batch_size=len(self.ds_test), shuffle=True)
+                                                       batch_size=self.bs_test, shuffle=True)
         
         self.loss_func = nn.NLLLoss()
 
     def load_all_data(self, device='cpu'):
-        self.X_train,self.Y_train = next(iter(self.loader_train))
-        self.X_test, self.Y_test = next(iter(self.loader_test))
-        self.X_train,self.Y_train = self.X_train.to(device), self.Y_train.to(device)
-        self.X_test, self.Y_test = self.X_test.to(device), self.Y_test.to(device)
-
+        data = [(Xb, Yb) for Xb, Yb in self.loader_train]
+        self.X_train = torch.cat([di[0] for di in data], dim=0).to(device)
+        self.Y_train = torch.cat([di[1] for di in data], dim=0).to(device)
+        
+        data = [(Xb, Yb) for Xb, Yb in self.loader_test]
+        self.X_test = torch.cat([di[0] for di in data], dim=0).to(device)
+        self.Y_test = torch.cat([di[1] for di in data], dim=0).to(device)
 
     def perform_stats(self, net, loader=None, show_stats=True, n_batches=-1, tqdm=tqdm, device='cpu'):
         if loader is None:
@@ -41,26 +45,34 @@ class MNIST:
                 break
             Y_batch_pred = net(X_batch)
             n_correct += (Y_batch_pred.argmax(dim=-1)==Y_batch).sum().item()
-            loss = loss_func(Y_batch_pred.log(), Y_batch).item()
+            loss = self.loss_func(Y_batch_pred.log(), Y_batch).item()
             loss_total += loss * len(X_batch)
             n_examples += len(X_batch)
             total += len(Y_batch)
         loss_total /= n_examples
-        accuracy = n_correct/total
+        accuracy = n_correct/total*100.
         if show_stats:
-            print(f'Average Loss: {loss_total:.03f}, Accuracy: {accuracy*100:.03f}%')
+            print(f'Average Loss: {loss_total:.03f}, Accuracy: {accuracy:.03f}%')
         return loss_total, accuracy
 
 
-    def calc_pheo_fitness(self, pheno, n_sample=1000, device='cpu'):
-        idx = torch.randperm(len(self.X_train))[:n_sample]
-        X_batch, Y_batch = self.X_train[idx].to(device), self.Y_train[idx].to(device)
+    def calc_pheo_fitness(self, pheno, n_sample=1000, device='cpu', ds='train'):
+        if ds=='train':
+            X, Y = self.X_train, self.Y_train
+        else:
+            X, Y = self.X_test, self.Y_test
+        if n_sample is None:
+            idx = torch.arange(len(X))
+        else:
+            idx = torch.randperm(len(X))[:n_sample]
+            
+        X_batch, Y_batch = X[idx].to(device), Y[idx].to(device)
 
         Y_batch_pred = pheno(X_batch)
         n_correct = (Y_batch_pred.argmax(dim=-1)==Y_batch).sum().item()
         loss = self.loss_func(Y_batch_pred.log(), Y_batch).item()
     #     if not np.isfinite(loss):
     #         loss = 10.
-        accuracy = n_correct/len(Y_batch)
+        accuracy = n_correct/len(Y_batch)*100.
         return {'fitness': -loss, 'loss': loss, 'accuracy': accuracy}
 
