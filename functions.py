36621b7d52c71e4ab278c2c891107a6631390557
Akarsh Kumar <akarshkumar0101@gmail.com>
2021-08-14 18:08:22 -0500

    add functions

--- functions.py
+++ functions.py
@@ -12,6 +12,51 @@ def rastrigin(x):
     d = x.shape[-1]
     ans = (x.pow(2.)-10*torch.cos(2*np.pi*x)).sum(dim=-1)
     return 10*d + ans
+
+def schaffer(x):
+    a0 = x.pow(2.).sum(dim=-1).sqrt().sin().pow(2.)
+    a1 = (x.pow(2.).sum(dim=-1)*1e-3+1).pow(2.)
+    return .5+(a0-.5)/a1
+
+def griewank(x):
+    """
+    −600< xi <600,
+    whose global minimum is f∗ = 0 at x∗ = (0, 0, ..., 0). This function is highly multimodal.
+    """
+    d = x.shape[-1]
+    i = torch.arange(d).float()+1
+    a0 = x.pow(2.).sum(dim=-1)/4000.
+    a1 = (x/i.sqrt()).cos().prod(dim=-1)
+    return a0-a1+1
+    
+
+def schwefel(x):
+    """
+    −500< xi <500,
+    whose global minimum f∗ ≈ −418.9829n 
+    occurs at xi = 420.9687 where i = 1, 2, ..., n.
+    """
+    return -(x*x.abs().sqrt().sin()).sum(dim=-1)
+
+def xin_she_yang(x):
+    return x.abs().sum(dim=-1)*(-x.pow(2.).sin().sum(dim=-1)).exp()
+
+def zakharov(x, n_terms=2):
+    d = x.shape[-1]
+    i = torch.arange(d).float()+1
+    
+    a0 = x.pow(2).sum(dim=-1)
+    
+    ab = (i*x).sum(dim=-1)/2.
+    a1 = [ab.pow(2*k) for k in range(n_terms)]
+    a1 = torch.stack(a1, dim=-1).sum(dim=-1)
+    return a0 + a1
+    
+    
+    
+    pass
+    
+
 def sphere(x):
     d = x.shape[-1]
     return x.pow(2.).sum(dim=-1)
